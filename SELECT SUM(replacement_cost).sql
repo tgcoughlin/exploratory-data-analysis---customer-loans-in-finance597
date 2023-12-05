cb69e1c5e1d5a92d4abac323708ceba75e157be5
Tom <tgcoughlin@gmail.com>
2023-12-05 17:41:39 +0000

    my first commit of code

---
+++ SELECT SUM(replacement_cost).sql
@@ -0,0 +1,2 @@
+SELECT SUM(replacement_cost) 
+FROM film;
---
+++ db_utils.py
@@ -0,0 +1,53 @@
+#import yaml
+#from sqlalchemy import create_engine
+import pandas as pd 
+
+#def load_credentials(file_path= 'credentials.yaml'):
+    #with open(file_path, 'r') as file:
+        #data = yaml.safe_load(file)
+    #return data
+
+#class RDSDatabaseConnector:
+    #def __init__(self, credentials) -> None:
+        #self.host = credentials.get('RDS_HOST')
+        #self.port = credentials.get('RDS_PORT')
+        #self.user = credentials.get('RDS_USER')
+        #self.password = credentials.get('RDS_PASSWORD')
+        #self.database = credentials.get('RDS_DATABASE')
+        #self.engine = None
+
+    #def initaliseSQLAlchemyEngine(self):
+        #self.engine = create_engine(f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}")
+    
+    #def connect(self):
+        #self.connection = self.engine.connect()
+    
+    #def extract_data(self):
+        #loan_df = pd.read_sql_table('loan_payments', self.engine)
+        #return loan_df
+    
+    #def save_data_as_CSV(self, dataframe, file_path):
+        #dataframe.to_csv(file_path, index=False)
+    
+    #def close_connection(self):
+        #self.connection.close()
+    
+
+#credentials_data = load_credentials()
+#rds_connnector = RDSDatabaseConnector(credentials_data)
+#rds_connnector.initaliseSQLAlchemyEngine()
+#rds_connnector.connect()
+#loan_table_df = rds_connnector.extract_data()
+#print(loan_table_df.head())
+#rds_connnector.save_data_as_CSV(loan_table_df, r'C:\Users\Tom\EDS_Project_Loan_Finance\loan_payments.csv')
+#rds_connnector.close_connection()
+
+def load_local_file(file_path):
+    local_df = pd.read_csv(file_path)
+    return local_df    
+
+
+finance_df = load_local_file('loan_payments.csv')   
+print(finance_df.head())
+print(finance_df.shape)
+print(finance_df.info())
